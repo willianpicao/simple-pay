b5c9e37c3926c43c0c73f11b1fb8d7a9fcaaf4fb
Willian <willianpicao6@gmail.com>
2025-06-21 10:07:51 -0400

    feat: create transfer entity

---
+++ src/main/java/br/com/willianpicao/picpay_simplificado/entity/Transfer.java
@@ -0,0 +1,34 @@
+package br.com.willianpicao.picpay_simplificado.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import br.com.willianpicao.picpay_simplificado.entity.users.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transfer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    BigDecimal value;
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id")
+    User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "payee_id")
+    User payee;
+
+    LocalDateTime moment;
+}
