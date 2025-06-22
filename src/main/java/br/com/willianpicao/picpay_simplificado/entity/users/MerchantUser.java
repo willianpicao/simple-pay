f6664426d49840799a7ada807a097d5c2dce49d3
Jessé Willian Picão <58005176+willianpicao@users.noreply.github.com>
2025-06-22 13:53:03 -0400

    refactor: add Lombok @NoArgsConstructor and @SuperBuilder to MerchantUser

--- src/main/java/br/com/willianpicao/picpay_simplificado/entity/users/MerchantUser.java
+++ src/main/java/br/com/willianpicao/picpay_simplificado/entity/users/MerchantUser.java
@@ -3,15 +3,19 @@ package br.com.willianpicao.picpay_simplificado.entity.users;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
-@DiscriminatorValue("MECHANT")
+@DiscriminatorValue("MERCHANT")
 @Getter
 @Setter
+@NoArgsConstructor
+@SuperBuilder
 public class MerchantUser extends User {
     @Override
-    public Boolean canSendMoney(){
+    public Boolean canSendMoney() {
         return false;
     }
 }
