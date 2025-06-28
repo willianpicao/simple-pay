f7a616e5f81ddb0f6cce932980fd3784dd049c99
Jessé Willian Picão <58005176+willianpicao@users.noreply.github.com>
2025-06-28 09:52:58 -0400

    feat: add DTO for AuthorizationClient response mapping

---
+++ src/main/java/br/com/willianpicao/picpay_simplificado/dto/AuthorizationData.java
@@ -0,0 +1,4 @@
+package br.com.willianpicao.picpay_simplificado.dto;
+
+public record AuthorizationData(Boolean authorization) {
+}
---
+++ src/main/java/br/com/willianpicao/picpay_simplificado/dto/AuthorizationResponse.java
@@ -0,0 +1,4 @@
+package br.com.willianpicao.picpay_simplificado.dto;
+
+public record AuthorizationResponse(String status, AuthorizationData data) {
+}
